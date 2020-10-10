d503de2758b600e89ef9870b028d789cedd3415d
Luis Solorzano <luisnic20@gmail.com>
2020-10-10 11:49:19 -0600

    Init Project Homeflow Date

---
+++ app/hfdatetime/src/main/java/com/homeflow/datetime/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.homeflow.datetime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.homeflow.datetime.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
