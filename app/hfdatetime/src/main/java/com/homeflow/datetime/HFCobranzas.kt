3f096c4d66ff2a57fe2dde176d35d1f996ddc542
Luis Solorzano <luisnic20@gmail.com>
2020-10-10 12:15:32 -0600

    Reformat code

--- app/hfdatetime/src/main/java/com/homeflow/datetime/HFCobranzas.kt
+++ app/hfdatetime/src/main/java/com/homeflow/datetime/HFCobranzas.kt
@@ -7,92 +7,95 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 object HFCobranzas {
-	/**
-	 * Fecha de la última cotización.
-	 * @var int plazo
-	 */
-	fun lastQuoteDate(plazo: Int): String {
-		val calendar = Calendar.getInstance()
-		calendar.time
-		val df = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-		var sunday = 0
+  /**
+   * Fecha de la última cotización.
+   * @var int plazo
+   */
+  fun lastQuoteDate(plazo: Int): String {
+    val calendar = Calendar.getInstance()
+    calendar.time
+    val df = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    var sunday = 0
 
-		for (i: Int in 1..plazo) {
-			val week = calendar.get(Calendar.DAY_OF_WEEK)
-			if (week == Calendar.SUNDAY) {
-				sunday += 1
-			}
-			calendar.add(Calendar.DATE, 1)
-		}
+    for (i: Int in 1..plazo) {
+      val week = calendar.get(Calendar.DAY_OF_WEEK)
+      if (week == Calendar.SUNDAY) {
+        sunday += 1
+      }
+      calendar.add(Calendar.DATE, 1)
+    }
 
-		calendar.add(Calendar.DATE, sunday)
-		val weekday = calendar.get(Calendar.DAY_OF_WEEK)
-		if (weekday == Calendar.SUNDAY) {
-			calendar.add(Calendar.DATE, 1)
-		}
+    calendar.add(Calendar.DATE, sunday)
+    val weekday = calendar.get(Calendar.DAY_OF_WEEK)
+    if (weekday == Calendar.SUNDAY) {
+      calendar.add(Calendar.DATE, 1)
+    }
 
-		return df.format(calendar.time)
-	}
+    return df.format(calendar.time)
+  }
 
-	/**
-	 * Obtener fecha de inicio y finalización sin meter Domingos.
-	 * @var int plazo
-	 * @var int periocicidad
-	 */
-	@SuppressLint("SimpleDateFormat")
-	fun getStartEndDate(plazo: Int, periodicidad: Int): MutableList<String> {
-		val startEndDate: MutableList<String> = ArrayList()
-		val df: DateFormat = SimpleDateFormat("yyyy-MM-dd")
-		val calendar = Calendar.getInstance()
-		calendar.time
-		for (i: Int in 1..plazo) {
-			val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
-			when (periodicidad) {
-          1 -> {
-              calendar.add(Calendar.DATE, if (dayOfWeek > 6) 9 - dayOfWeek else 1)
-          }
-				else -> {
-					calendar.add(
-              Calendar.DATE,
-              if (dayOfWeek == Calendar.SATURDAY) periodicidad + 3 else periodicidad
+  /**
+   * Obtener fecha de inicio y finalización sin meter Domingos.
+   * @var int plazo
+   * @var int periocicidad
+   */
+  @SuppressLint("SimpleDateFormat")
+  fun getStartEndDate(plazo: Int, periodicidad: Int): MutableList<String> {
+    val startEndDate: MutableList<String> = ArrayList()
+    val df: DateFormat = SimpleDateFormat("yyyy-MM-dd")
+    val calendar = Calendar.getInstance()
+    calendar.time
+    for (i: Int in 1..plazo) {
+      val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+      when (periodicidad) {
+        1 -> {
+          calendar.add(Calendar.DATE, if (dayOfWeek > 6) 9 - dayOfWeek else 1)
+        }
+        else -> {
+          calendar.add(
+            Calendar.DATE,
+            if (dayOfWeek == Calendar.SATURDAY) periodicidad + 3 else periodicidad
           )
-				}
-			}
+        }
+      }
 
-			if (i == 1) {
-				startEndDate.add(df.format(calendar.time))
-			} else if (i == plazo) {
-				startEndDate.add(df.format(calendar.time))
-			}
-		}
-		return startEndDate
-	}
+      if (i == 1) {
+        startEndDate.add(df.format(calendar.time))
+      } else if (i == plazo) {
+        startEndDate.add(df.format(calendar.time))
+      }
+    }
+    return startEndDate
+  }
 
-	/**
-	 * Obtener plan de pago.
-	 * @var int plazo
-	 * @var int periocicidad
-	 */
-	@SuppressLint("SimpleDateFormat")
-	fun getPaymentPlan(plazo: Int, periodicidad: Int): MutableList<String> {
-		val paymentPlan: MutableList<String> = ArrayList()
-		val df: DateFormat = SimpleDateFormat("yyyy-MM-dd")
-		val calendar = Calendar.getInstance()
-		calendar.time
-		for (i: Int in 1..plazo) {
-			val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
-			when (periodicidad) {
-          1 -> {
-              calendar.add(Calendar.DATE, if (dayOfWeek > 6) 9 - dayOfWeek else 1)
-              paymentPlan.add(df.format(calendar.time))
-          }
-				else -> {
-            calendar.add(Calendar.DATE, if (dayOfWeek == Calendar.SATURDAY) periodicidad + 3 else periodicidad)
-            paymentPlan.add(df.format(calendar.time))
-				}
-			}
-		}
+  /**
+   * Obtener plan de pago.
+   * @var int plazo
+   * @var int periocicidad
+   */
+  @SuppressLint("SimpleDateFormat")
+  fun getPaymentPlan(plazo: Int, periodicidad: Int): MutableList<String> {
+    val paymentPlan: MutableList<String> = ArrayList()
+    val df: DateFormat = SimpleDateFormat("yyyy-MM-dd")
+    val calendar = Calendar.getInstance()
+    calendar.time
+    for (i: Int in 1..plazo) {
+      val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+      when (periodicidad) {
+        1 -> {
+          calendar.add(Calendar.DATE, if (dayOfWeek > 6) 9 - dayOfWeek else 1)
+          paymentPlan.add(df.format(calendar.time))
+        }
+        else -> {
+          calendar.add(
+            Calendar.DATE,
+            if (dayOfWeek == Calendar.SATURDAY) periodicidad + 3 else periodicidad
+          )
+          paymentPlan.add(df.format(calendar.time))
+        }
+      }
+    }
 
-		return paymentPlan
-	}
+    return paymentPlan
+  }
 }
